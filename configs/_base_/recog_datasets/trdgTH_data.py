9df73fa98f144194f24683d262c63d75a8e98ea3
Komson <shootingstar7447@gmail.com>
2022-06-06 10:55:11 +0700

    Fix test config

--- configs/_base_/recog_datasets/trdgTH_data.py
+++ configs/_base_/recog_datasets/trdgTH_data.py
@@ -5,7 +5,7 @@ img_prefix = f'{root}/imgs'
 train_anno_file1 = f'{root}/label.txt'
 test_anno_file1 = f'{root}/test_images/0.jpg'
 
-train=dict(
+train = dict(
     type=dataset_type,                  # dataset name
     ann_file=f'{root}/train_images/label.txt',  # Path to annotation file
     img_prefix=f'{root}/train_images',  # Path to images
@@ -20,15 +20,17 @@ train=dict(
     pipeline=None)
 
 test = dict(
-    type=dataset_type,
-    img_prefix=img_prefix,
-    ann_file=test_anno_file1,
+    type=dataset_type,                  # dataset name
+    ann_file=f'{root}/test_images/label.txt',  # Path to annotation file
+    img_prefix=f'{root}/test_images',  # Path to images
     loader=dict(
         type='AnnFileLoader',
-        repeat=1,
-        file_format='lmdb',
-        file_storage_backend='disk',
-        parser=dict(type='LineJsonParser', keys=['filename', 'text'])),
+        # repeat=10,
+        parser=dict(
+            type='LineStrParser',
+            keys=['filename', 'text'],
+            keys_idx=[0, 1],
+            separator=' ')),
     pipeline=None,
     test_mode=True)
 
