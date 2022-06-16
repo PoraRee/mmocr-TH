7f3f19f8465a94682b7ba5986248496827cc9747
Komson <shootingstar7447@gmail.com>
2022-06-16 13:44:02 +0700

    New trdg dataset

--- configs/_base_/recog_datasets/trdgTH_data.py
+++ configs/_base_/recog_datasets/trdgTH_data.py
@@ -2,26 +2,39 @@ dataset_type = 'OCRDataset'
 
 root = './mmocr/data/trdgTH'
 img_prefix = f'{root}/imgs'
-train_anno_file1 = f'{root}/label.txt'
-test_anno_file1 = f'{root}/test_images/0.jpg'
+train_anno_file1 = f'{root}/train_images'
+train_anno_file2 = f'{root}/train_images_2'
+train_anno_file3 = f'{root}/google_ocr_train_images'
+test_anno_file1 = f'{root}/test_images'
+test_anno_file2 = f'{root}/google_ocr_test_images'
 
-# train = dict(
-#     type=dataset_type,
-#     ann_file=f'{root}/train_images/label.txt',
-#     img_prefix=f'{root}/train_images',
-#     loader=dict(
-#         type='AnnFileLoader',
-#         parser=dict(
-#             type='LineStrParser',
-#             keys=['filename', 'text'],
-#             keys_idx=[0, 1],
-#             separator=' ')),
-#     pipeline=None)
 
-train = dict(
+train1 = dict(
     type=dataset_type,
-    ann_file=f'{root}/train_images/label.txt',
-    img_prefix=f'{root}/train_images',
+    ann_file=f'{train_anno_file1}/label.txt',
+    img_prefix=train_anno_file1,
+    loader=dict(
+        type='AnnFileLoader',
+        parser=dict(
+            type='LineJsonParser',
+            keys=['filename', 'text'],
+        )),
+    pipeline=None)
+train2 = dict(
+    type=dataset_type,
+    ann_file=f'{train_anno_file2}/label.txt',
+    img_prefix=train_anno_file2,
+    loader=dict(
+        type='AnnFileLoader',
+        parser=dict(
+            type='LineJsonParser',
+            keys=['filename', 'text'],
+        )),
+    pipeline=None)
+train3 = dict(
+    type=dataset_type,
+    ann_file=f'{train_anno_file3}/label.txt',
+    img_prefix=train_anno_file3,
     loader=dict(
         type='AnnFileLoader',
         parser=dict(
@@ -30,10 +43,23 @@ train = dict(
         )),
     pipeline=None)
 
-test = dict(
+test1 = dict(
     type=dataset_type,
-    ann_file=f'{root}/test_images/label.txt',
-    img_prefix=f'{root}/test_images',
+    ann_file=f'{test_anno_file1}/label.txt',
+    img_prefix=f'{test_anno_file1}',
+    loader=dict(
+        type='AnnFileLoader',
+        parser=dict(
+            type='LineStrParser',
+            keys=['filename', 'text'],
+            keys_idx=[0, 1],
+            separator=' ')),
+    pipeline=None,
+    test_mode=True)
+test2 = dict(
+    type=dataset_type,
+    ann_file=f'{test_anno_file2}/label.txt',
+    img_prefix=f'{test_anno_file2}',
     loader=dict(
         type='AnnFileLoader',
         parser=dict(
@@ -45,6 +71,6 @@ test = dict(
     test_mode=True)
 
 
-train_list = [train]
+train_list = [train1, train2, train3]
 
-test_list = [test]
+test_list = [test1, test2]
