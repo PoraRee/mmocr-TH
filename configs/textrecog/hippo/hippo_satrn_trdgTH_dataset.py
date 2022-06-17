2eff77e9ab66aed40e334d70329fd7a071796194
Komson <shootingstar7447@gmail.com>
2022-06-17 11:02:36 +0700

    New satrn model

--- configs/textrecog/hippo/hippo_satrn_trdgTH_dataset.py
+++ configs/textrecog/hippo/hippo_satrn_trdgTH_dataset.py
@@ -1,9 +1,8 @@
 _base_ = [
     '../../_base_/default_runtime.py',
-    '../../_base_/recog_pipelines/hippo_satrn_pipeline.py',
-    '../../_base_/schedules/schedule_adam_step_5e.py',
+    '../../_base_/recog_pipelines/satrn_pipeline.py',
     '../../_base_/recog_datasets/trdgTH_data.py',
-
+    '../../_base_/schedules/schedule_adam_step_12e.py',
 ]
 
 train_list = {{_base_.train_list}}
@@ -17,38 +16,38 @@ label_convertor = dict(
     dict_list=list('0123456789abcdefghijklmnopqrstuvwxyz'
                    'ABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()'
                    '*+,-./:;<=>?@[\\]_`~'
-                   'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮฤฦะัาำิีึืุูเแโใไๅํ็่้๊๋ฯฺๆ์๎๏๚๛๐๑๒๓๔๕๖๗๘๙฿'),
+                   'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮฤฦะัาำิีึืุูเแโใไๅํ็่้๊๋ฯฺๆ์๎๏๚๛๐๑๒๓๔๕๖๗๘๙฿ '),
     with_unknown=True)
 
 model = dict(
     type='SATRN',
-    backbone=dict(type='ShallowCNN', input_channels=3, hidden_dim=512),
+    backbone=dict(type='ShallowCNN', input_channels=3, hidden_dim=256),
     encoder=dict(
         type='SatrnEncoder',
-        n_layers=12,
+        n_layers=6,
         n_head=8,
-        d_k=512 // 8,
-        d_v=512 // 8,
-        d_model=512,
+        d_k=256 // 8,
+        d_v=256 // 8,
+        d_model=256,
         n_position=100,
-        d_inner=512 * 4,
+        d_inner=256 * 4,
         dropout=0.1),
     decoder=dict(
         type='NRTRDecoder',
         n_layers=6,
-        d_embedding=512,
+        d_embedding=256,
         n_head=8,
-        d_model=512,
-        d_inner=512 * 4,
-        d_k=512 // 8,
-        d_v=512 // 8),
+        d_model=256,
+        d_inner=256 * 4,
+        d_k=256 // 8,
+        d_v=256 // 8),
     loss=dict(type='TFLoss'),
     label_convertor=label_convertor,
-    max_seq_len=25)
+    max_seq_len=80)
 
 
 data = dict(
-    samples_per_gpu=8,
+    samples_per_gpu=32,
     workers_per_gpu=4,
     val_dataloader=dict(samples_per_gpu=1),
     test_dataloader=dict(samples_per_gpu=1),
