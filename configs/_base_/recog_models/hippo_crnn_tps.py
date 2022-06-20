b69b61f6db164af5cc32024909ccaac9c9a1576c
Komson <shootingstar7447@gmail.com>
2022-06-20 11:21:07 +0700

    Add crnn tps

---
+++ configs/_base_/recog_models/hippo_crnn_tps.py
@@ -0,0 +1,22 @@
+label_convertor = dict(
+    type='CTCConvertor',
+    dict_list=list('0123456789abcdefghijklmnopqrstuvwxyz'
+                   'ABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()'
+                   '*+,-./:;<=>?@[\\]_`~'
+                   'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮฤฦะัาำิีึืุูเแโใไๅํ็่้๊๋ฯฺๆ์๎๏๚๛๐๑๒๓๔๕๖๗๘๙฿ '),
+    with_unknown=True)
+
+model = dict(
+    type='CRNNNet',
+    preprocessor=dict(
+        type='TPSPreprocessor',
+        num_fiducial=20,
+        img_size=(32, 100),
+        rectified_img_size=(32, 100),
+        num_img_channel=1),
+    backbone=dict(type='VeryDeepVgg', leaky_relu=False, input_channels=1),
+    encoder=None,
+    decoder=dict(type='CRNNDecoder', in_channels=512, rnn_flag=True),
+    loss=dict(type='CTCLoss'),
+    label_convertor=label_convertor,
+    pretrained=None)
